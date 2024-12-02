79d271c2b8c9940870be57616fafec8007897a18
Ecoleplus <743038-ecoleplus@users.noreply.gitlab.com>
2024-12-02 11:03:47 +0100

    fixes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Ecoleplus\EcoleplusUi\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Ecoleplus\EcoleplusUi\EcoleplusUiServiceProvider;
 
@@ -12,9 +11,6 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Ecoleplus\\EcoleplusUi\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -26,11 +22,7 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_ecoleplus-ui_table.php.stub';
-        $migration->up();
-        */
+
     }
 }
